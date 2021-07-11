0aca9ad69da63b0ff55d34cc201015eebdda6e9b
phongvu0403 <phongvubuffalo2344@gmail.com>
2021-07-11 19:56:04 +0700

    add static file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var path = require('path');
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
